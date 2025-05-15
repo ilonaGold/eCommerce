7b8a5ee9c7651e31f90503cca35052bfc44d2bd3
anakdrch <ana.kudrevich@gmail.com>
2025-05-15 16:10:55 +0200

    feat: implement renderView and routeHandler functions

---
+++ src/routing/router.ts
@@ -0,0 +1,52 @@
+import { Routes } from "../interfaces/interfaces";
+import { getState, setView } from "../state/state";
+import { renderLogin } from "../views/login";
+import { renderMain } from "../views/main";
+import { renderRegistration } from "../views/registration";
+import { renderError } from "../views/error";
+
+const routes: Routes = {
+  "": "Log In",
+  login: "Log In",
+  main: "Main",
+  registration: "Registration",
+};
+
+export function routeHandler(): void {
+  const path = location.pathname;
+  const LAST_INDEX = -1;
+  const arr = path.split("/").filter(Boolean);
+  const endpoint = arr.at(LAST_INDEX);
+  const isAuth = getState("userAuth");
+  if (endpoint && routes[endpoint]) {
+    if (!isAuth) {
+      setView(endpoint);
+    } else {
+      setView("main");
+    }
+  } else {
+    setView("error");
+  }
+  renderView();
+}
+
+function renderView(): void {
+  const view = getState("view");
+  const root = document.querySelector("#root");
+  if (root) root.innerHTML = "";
+  switch (view) {
+    case "":
+    case "login":
+      renderLogin();
+      break;
+    case "registration":
+      renderRegistration();
+      break;
+    case "main":
+      renderMain();
+      break;
+    default:
+      renderError();
+      break;
+  }
+}
