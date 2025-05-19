0b1bf4595afddd8372bc3f51f4eca9d14a0a1ef0
anakdrch <ana.kudrevich@gmail.com>
2025-05-19 17:43:25 +0200

    feat: redirect from registration to login if user has an account

--- src/views/renderRegistration/renderRegistration.ts
+++ src/views/renderRegistration/renderRegistration.ts
@@ -1,4 +1,5 @@
 import { createRegistrationForm } from "../../components/registration/registrationForm";
+import { goToView } from "../../routing/router";
 import { createElement } from "../../utils/dom/createElement";
 
 import "./registration.css";
@@ -6,9 +7,12 @@ import "./registration.css";
 export const renderRegistration = (parent: HTMLElement): void => {
   const registerForm = createRegistrationForm();
 
-  const toLoginBtn = createElement("button", { type: "button", class: "login-redirect-btn" }, [
-    "Already have an account? Login",
-  ]);
+  const toLoginBtn = createElement(
+    "button",
+    { type: "button", class: "login-redirect-btn" },
+    ["Already have an account? Login"],
+    { events: { click: () => goToView("login") } }
+  );
 
   const registerFormContainer = createElement("div", { class: "register-container" }, [
     registerForm,
