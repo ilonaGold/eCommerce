e35d9e692267d2bad17d087a8897bd6008f96efb
anakdrch <ana.kudrevich@gmail.com>
2025-05-19 16:27:24 +0200

    feat: implement togglePasswordVisibility function

--- src/components/login/loginForm.ts
+++ src/components/login/loginForm.ts
@@ -1,5 +1,6 @@
 import { createElement } from "../../utils/dom/createElement";
 import { createInputGroup, createSectionTitle } from "../../utils/dom/form/createInputGroup";
+import { togglePasswordVisibility } from "../../utils/dom/form/togglePasswordVisibility";
 import { loginFormInit } from "./helpers/loginFormInit";
 import { validateLoginForm } from "./helpers/loginFormValidation";
 
@@ -8,9 +9,11 @@ export function createLoginForm(): HTMLFormElement {
     createSectionTitle("Sign In"),
     createInputGroup("Email", "email", "email", true),
     createInputGroup("Password", "password", "password", true),
+    createInputGroup("Show password", "checkbox", "showPassword"),
     createElement("button", { type: "submit" }, ["Sign In"]),
   ]);
 
+  togglePasswordVisibility(form);
   validateLoginForm(form);
   loginFormInit(form);
 
