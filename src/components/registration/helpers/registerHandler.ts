608b2cc8b405589abc0526773185f41c59e971a2
beqa <beqarioni@yahoo.co.uk>
2025-05-23 21:40:46 +0400

    fix: during register same credentials wont let you login again

--- src/components/registration/helpers/registerHandler.ts
+++ src/components/registration/helpers/registerHandler.ts
@@ -4,6 +4,7 @@ import { loginCustomer } from "../../../services/auth/loginCustomer";
 import { registerCustomer } from "../../../services/auth/registerCustomer";
 import { storeLoginData } from "../../../services/localStorage/localStorage";
 import { setState } from "../../../state/state";
+import { notificationModal } from "../../notificationModal/notificationModal";
 import { createCustomerDraft } from "./createCustomerDraft";
 
 export async function registerHandler(event: Event): Promise<void> {
@@ -14,19 +15,27 @@ export async function registerHandler(event: Event): Promise<void> {
     return;
   }
 
-  const data = createCustomerDraft(form);
-  const { email, password } = data;
+  try {
+    const data = createCustomerDraft(form);
+    const { email, password } = data;
 
-  await registerCustomer(data);
-  const customerData = (await loginCustomer(email, password)) as LoginInfo;
-  console.log(
-    "After registration, log in happens also, but 'silently', because we need toredirect after"
-  );
-  // +++++++++++++++++++++++   Updatind state / Saving in localStorage (needs separate module)  ++++++++++++++++++++++++++++++
-  setState("userAuth", true);
-  setState("customer", customerData.customer);
-  storeLoginData(JSON.stringify(customerData));
-  navigateTo("/");
-  // +++++++++++++++++++++++   Updatind state / Saving in localStorage (needs separate module)   ++++++++++++++++++++++++++++++
-  navigateTo("/");
+    await registerCustomer(data);
+    const customerData = (await loginCustomer(email, password)) as LoginInfo;
+    console.log(
+      "After registration, log in happens also, but 'silently', because we need toredirect after"
+    );
+    // +++++++++++++++++++++++   Updatind state / Saving in localStorage (needs separate module)  ++++++++++++++++++++++++++++++
+    setState("userAuth", true);
+    setState("customer", customerData.customer);
+    storeLoginData(customerData);
+    navigateTo("/");
+    // +++++++++++++++++++++++   Updatind state / Saving in localStorage (needs separate module)   ++++++++++++++++++++++++++++++
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      notificationModal(error.message, "error");
+    } else {
+      console.log(error);
+    }
+  }
 }
