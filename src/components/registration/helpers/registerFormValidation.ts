0fff3dd98557ccbc0d76cd0a0ef926c2aaefcfc0
anakdrch <ana.kudrevich@gmail.com>
2025-05-23 16:45:23 +0200

    feat: implement validateRegistrationForm function

--- src/components/registration/helpers/registerFormValidation.ts
+++ src/components/registration/helpers/registerFormValidation.ts
@@ -1,12 +1,37 @@
-import { updateError } from "../../../utils/dom/form/inputValidation";
+import { Fields } from "../../../interfaces/interfaces";
+import {
+  validateAge,
+  validateCountry,
+  validateEmail,
+  validatePassword,
+  validateRepeatPssw,
+  validatePostcode,
+  validateStreet,
+  validateString,
+  addValidation,
+} from "../../../utils/dom/form/inputValidation";
 
-export function addValidation(
-  input: HTMLInputElement,
-  validate: (input: HTMLInputElement) => string,
-  divError: HTMLDivElement
-): void {
-  input.addEventListener("input", () => {
-    const text = validate(input);
-    updateError({ fieldInput: input, fieldError: divError, text: text });
-  });
+export function validateRegistrationForm(form: HTMLFormElement): void {
+  form.noValidate = true;
+  const fields: Fields = {
+    email: validateEmail,
+    firstName: validateString,
+    lastName: validateString,
+    password: validatePassword,
+    repeatPassword: validateRepeatPssw,
+    dateOfBirth: validateAge,
+    address: validateStreet,
+    city: validateString,
+    country: validateCountry,
+    postalCode: validatePostcode,
+    billingAddress: validateStreet,
+    billingCity: validateString,
+    billingCountry: validateCountry,
+    billingPostalCode: validatePostcode,
+  };
+  for (const field in fields) {
+    const input: HTMLInputElement | null = form.querySelector(`#${field}`);
+    const error: HTMLDivElement | null = form.querySelector(`#${field}Error`);
+    if (input && error) addValidation({ input: input, validate: fields[field], divError: error });
+  }
 }
