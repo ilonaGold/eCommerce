94f925c4d2f7d7a64b32834a884363c43fe38dec
anakdrch <ana.kudrevich@gmail.com>
2025-05-25 10:39:50 +0200

    feat: implement renderError function

--- src/views/renderError/renderError.ts
+++ src/views/renderError/renderError.ts
@@ -1,3 +1,25 @@
+import { createFooter } from "../../components/footer/footer";
+import { createElement } from "../../utils/dom/createElement";
+
 export function renderError(parent: HTMLElement): void {
-  console.log(`Error page appends to ${parent}`);
+  const info = createElement("div", { class: "error-info" }, [
+    createElement("h3", { class: "error-header" }, ["Page not found"]),
+    createElement("p", { class: "error-text" }, [
+      "Sorry, we couldn't find the page you're looking for",
+    ]),
+    createElement("button", { class: "error-button" }, ["Go back to home"]),
+  ]);
+
+  const picture = createElement("div", { class: "error-info" }, [
+    createElement("img", {
+      src: "../../assets/images/panda-forest.png",
+      alt: "Forest panda",
+      class: "panda-image",
+    }),
+  ]);
+
+  const section = createElement("section", { class: "error-section" }, [info, picture]);
+
+  const container = createElement("main", { class: "error-main" }, [section, createFooter()]);
+  parent.append(container);
 }
