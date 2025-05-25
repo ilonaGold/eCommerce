a0124ef6353395eaa9623cd196b08639774c92d7
anakdrch <ana.kudrevich@gmail.com>
2025-05-25 18:50:27 +0200

    fix: adjust home page layout and styles

--- src/views/renderHome/renderHome.ts
+++ src/views/renderHome/renderHome.ts
@@ -7,9 +7,15 @@ import "./renderHome.css";
 export function renderHome(parent: HTMLElement): void {
   const isAuth = getState("userAuth");
   const customer = getState("customer");
-  const main = createElement("main", { class: "home-main" }, [
-    createElement("p", { class: "home-text" }, ["Relax, while we're building this page for you"]),
+  const imageContainer = createElement("div", { class: "home-image-container" }, [
+    createElement("img", {
+      src: "../../assets/images/sleeping-panda.png",
+      alt: "Home panda",
+      class: "home-panda-image",
+    }),
+    createElement("p", { class: "home-text" }, ["Relax, while we're building home page for you"]),
   ]);
+  const main = createElement("main", { class: "home-main" }, [imageContainer]);
   const container = createElement("main", { class: "home-container" }, [
     createHeader(isAuth, customer),
     main,
