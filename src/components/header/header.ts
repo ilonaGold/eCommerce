67f2710ab5d5713ff996aacc95111a0128330a42
anakdrch <ana.kudrevich@gmail.com>
2025-05-25 20:16:16 +0200

    fix: fix layout when burger menu is open

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -91,13 +91,11 @@ export function createHeader(isLoggedIn: boolean, customer: Customer | null): HT
   const hamburgerMenu = navigation.querySelector(".hamburger-menu");
   hamburgerMenu?.addEventListener("click", () => {
     const navLinks = document.querySelector(".nav-links");
-    const welcomeText = document.querySelector(".welcome-text");
     const mainContent = document.querySelector(".main-content");
     const text = document.querySelector(".page-text");
 
     navLinks?.classList.toggle("active");
     hamburgerMenu.classList.toggle("active");
-    welcomeText?.classList.toggle("menu-open");
     mainContent?.classList.toggle("nav-open");
     text?.classList.toggle("open");
   });
