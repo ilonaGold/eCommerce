1b5a824ebc12359337e95506f3a2078cc8cbb992
anakdrch <ana.kudrevich@gmail.com>
2025-06-02 15:56:01 +0200

    fix: fix route where to redirect from protected routes

--- src/routing/router.ts
+++ src/routing/router.ts
@@ -109,7 +109,7 @@ export function routeHandler(): void {
         route.handle();
       } else {
         if (protectedRoutes.includes(path)) {
-          history.replaceState(null, "", "main");
+          history.replaceState(null, "", "/main");
           setView("main");
           renderMain(getRoot());
         } else {
@@ -127,101 +127,3 @@ export function goToView(view: string): void {
   history.pushState({}, "", `/${view}`);
   routeHandler();
 }
-
-/*const routes: Routes = {
-  "/": "Main",
-  main: "Main",
-  login: "Log In",
-  registration: "Registration",
-  home: "Home",
-  about: "About",
-  contacts: "Contacts",
-};
-
-const restrictedRoutes = ["login", "registration"];
-
-export function routeHandler(): void {
-  const path = location.pathname;
-  const LAST_INDEX = -1;
-  const arr = path.split("/").filter(Boolean);
-  let endpoint: string | undefined;
-  if (arr.length) {
-    endpoint = arr.at(LAST_INDEX);
-  } else {
-    endpoint = "/";
-  }
-  const isAuth = getState("userAuth");
-  console.log(isAuth);
-
-  setAuth(isAuth);
-  if (endpoint && routes[endpoint]) {
-    if (!isAuth) {
-      setView(endpoint);
-    } else {
-      if (restrictedRoutes.includes(endpoint)) {
-        const newPath = createNewPath("main");
-        history.replaceState(null, "", newPath);
-        setView("main");
-      } else {
-        setView(endpoint);
-      }
-    }
-  } else {
-    setView("error");
-  }
-  renderView();
-}
-
-function renderView(): void {
-  const view = getState("view");
-  let root: HTMLElement | null = document.querySelector("#root");
-  if (root) {
-    root.innerHTML = "";
-  } else {
-    root = createElement("div", { id: "root" });
-    document.body.append(root);
-  }
-  switch (view) {
-    case "/":
-    case "main":
-      renderMain(root);
-      break;
-    case "login":
-      renderLogin(root);
-      break;
-    case "registration":
-      renderRegistration(root);
-      break;
-    case "home":
-      renderHome(root);
-      break;
-    case "about":
-      renderAbout(root);
-      break;
-    case "contacts":
-      renderContacts(root);
-      break;
-    default:
-      renderError(root);
-      break;
-  }
-*/
-
-/*export function goToView(view: string): void {
-  const newPath = createNewPath(view);
-  history.pushState({}, "", newPath);
-  routeHandler();
-}
-
-export function createNewPath(view: string): string {
-  const path = location.pathname;
-  const arr = path.split("/").filter(Boolean);
-  if (!arr.length) {
-    arr.push(view);
-  } else {
-    arr[arr.length - 1] = view;
-  }
-  const newPath = `/${arr.join("/")}`;
-  return newPath;
-}
-*/
