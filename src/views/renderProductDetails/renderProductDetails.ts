da725302fc09b8179d9eefa39e8369d347084c2f
anakdrch <ana.kudrevich@gmail.com>
2025-06-04 17:51:33 +0200

    feat: remove dummy renderProductDetails function and implement a proper page rendering function

--- src/views/renderProductDetails/renderProductDetails.ts
+++ src/views/renderProductDetails/renderProductDetails.ts
@@ -1,6 +1,23 @@
+import { description } from "../../components/productDetails/description/description";
 import { ProductDetailsProps } from "../../interfaces/interfaces";
+import { getState } from "../../state/state";
+import { createElement } from "../../utils/dom/createElement";
+import { createHeader } from "../../components/header/header";
+import { createFooter } from "../../components/footer/footer";
+import { mainComponent } from "../../components/main/main";
 
 export function renderProductDetails({ parent, product }: ProductDetailsProps): void {
-  console.log(`Dummy page appends to ${parent}`);
-  console.log(product?.results);
+  const isAuth = getState("userAuth");
+  const customer = getState("customer");
+
+  const descField = product ? description(product) : createElement("div");
+  const header = createHeader(isAuth, customer);
+  const main = mainComponent(descField);
+  const footer = createFooter();
+  const viewContainer = createElement("div", { class: "product-details-page view-container" }, [
+    header,
+    main,
+    footer,
+  ]);
+  parent.append(viewContainer);
 }
