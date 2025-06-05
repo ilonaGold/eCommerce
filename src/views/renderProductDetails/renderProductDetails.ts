a4f1aa3f2f9330b4b3b6f52fa5e123610f863c83
anakdrch <ana.kudrevich@gmail.com>
2025-06-05 22:54:23 +0200

    feat: implement slider component with basic styles

--- src/views/renderProductDetails/renderProductDetails.ts
+++ src/views/renderProductDetails/renderProductDetails.ts
@@ -1,4 +1,5 @@
 import { description } from "../../components/productDetails/description/description";
+import { imageSlider } from "../../components/productDetails/slider/slider";
 import { ProductDetailsProps } from "../../interfaces/interfaces";
 import { getState } from "../../state/state";
 import { createElement } from "../../utils/dom/createElement";
@@ -6,13 +7,20 @@ import { createHeader } from "../../components/header/header";
 import { createFooter } from "../../components/footer/footer";
 import { mainComponent } from "../../components/main/main";
 
+import "./renderProductDetails.css";
+
 export function renderProductDetails({ parent, product }: ProductDetailsProps): void {
   const isAuth = getState("userAuth");
   const customer = getState("customer");
 
   const descField = product ? description(product) : createElement("div");
+  const slider = product ? imageSlider(product) : createElement("div");
+  const detailsContainer = createElement("div", { class: "product-details_container" }, [
+    slider,
+    descField,
+  ]);
   const header = createHeader(isAuth, customer);
-  const main = mainComponent(descField);
+  const main = mainComponent(detailsContainer);
   const footer = createFooter();
   const viewContainer = createElement("div", { class: "product-details-page view-container" }, [
     header,
