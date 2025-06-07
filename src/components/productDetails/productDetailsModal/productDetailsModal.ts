9c258e90728a7b84e4824ffebf971969abfc17f0
anakdrch <ana.kudrevich@gmail.com>
2025-06-07 16:12:36 +0200

    feat: implement modal with image slider for product details page

---
+++ src/components/productDetails/productDetailsModal/productDetailsModal.ts
@@ -0,0 +1,19 @@
+import { ProductProjection } from "../../../interfaces/products/ProductProjection";
+import { createElement } from "../../../utils/dom/createElement";
+import { imageSlider } from "../slider/slider";
+
+import "./productDetailsModal.css";
+import "./slider.css";
+
+export const modalWithSlider = (product: ProductProjection): void => {
+  const modalOverlay = createElement("div", { class: "modal-overlay" });
+  const modalWindow = createElement("div", { class: "modal-window" });
+  modalOverlay.append(modalWindow);
+  const slider = imageSlider(product);
+  const closeButton = createElement("button", { class: "close-button" }, ["x"]);
+  closeButton.addEventListener("click", () => {
+    document.body.removeChild(modalOverlay);
+  });
+  modalWindow.append(slider, closeButton);
+  document.body.append(modalOverlay);
+};
