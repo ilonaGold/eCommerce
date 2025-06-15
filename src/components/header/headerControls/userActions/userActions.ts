c40e0cc87e26f29687b0737e2941f22dee47fcf9
beqa <beqarioni@yahoo.co.uk>
2025-06-15 19:16:07 +0400

    refactor: move user account part of the code as separate module

--- src/components/header/headerControls/userActions/userActions.ts
+++ src/components/header/headerControls/userActions/userActions.ts
@@ -1,51 +1,17 @@
 import { Customer } from "../../../../interfaces/dataInterfaces";
 import { goToView } from "../../../../routing/router";
-import { clearLoginData } from "../../../../services/localStorage/localStorage";
-import { setAuth, setCustomer } from "../../../../state/state";
 import { createElement } from "../../../../utils/dom/createElement";
+import { createUserAccountComponent } from "./userAccount/userAccount";
 
 import "./userActions.css";
 
 export const createUserActions = (isLoggedIn: boolean, customer: Customer | null): HTMLElement => {
-  // logout button
-  const logOutBtn = createElement("a", { class: "logout-btn" }, ["Log Out"], {
-    events: {
-      click: (e) => {
-        e.preventDefault();
-        setAuth(false);
-        setCustomer(null);
-        clearLoginData();
-        goToView("");
-      },
-    },
-    styles: {
-      cursor: "pointer",
-    },
-  });
-
-  // User profile button
-  const userProfileBtn = createElement("a", { class: "user-profile-link" }, ["User Profile"], {
-    events: {
-      click: (e) => {
-        e.preventDefault();
-        goToView("user-profile");
-      },
-    },
-    styles: {
-      cursor: "pointer",
-    },
-  });
+  const userAccount = createUserAccountComponent(customer);
 
   const headerButtonChildren = isLoggedIn
     ? [
         // user specific
-        createElement("div", { class: "user-account" }, [
-          createElement("div", { class: "welcome-message" }, [
-            `Welcome, `,
-            createElement("span", { class: "user-name" }, [customer?.firstName || "User"]),
-          ]),
-          createElement("div", { class: "user-controls" }, [userProfileBtn, logOutBtn]),
-        ]),
+        userAccount,
       ]
     : [
         // login/register
