8038922af5c49cd89d22aeb1e04b3ae296cb0471
anakdrch <ana.kudrevich@gmail.com>
2025-06-19 20:15:28 +0200

    feat: add file for checkMyActiveCart.ts

---
+++ src/services/API/cart/checkMyActiveCart.ts
@@ -0,0 +1,8 @@
+/*import { getAnonymousAccessTokenData } from "../../auth/getAnonymousAccessToken";
+import { getCustomerAccessTokenData } from "../../auth/getCustomerAccessToken";
+import { isCustomerLoggedIn } from "../../auth/isCustomerLoggedIn";
+
+export const checkMyActiveCart = async (): Promise<Response> => {
+  const url = `${import.meta.env.VITE_CTP_AUTH_URL}/${import.meta.env.VITE_CTP_PROJECT_KEY}/me/active-cart`;
+}
+*/
